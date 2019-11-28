737a63e27420b2d129db478215d6b2899bb1553c
PvtPnt <44025250+PvtPnt@users.noreply.github.com>
2019-11-28 13:53:49 +0700

    Unity version update + Camera adjustment

--- Assets/Script/Camera.cs
+++ Assets/Script/Camera.cs
@@ -9,7 +9,10 @@ public class Camera : MonoBehaviour
     public Vector3 Normal_Offset;
     public Vector3 Current_Offset;
     public Vector3 High_Offset;
-    public float SmoothDamp = 0.125f;
+
+    public float Current_SmoothDamp;
+    public float High_Damp;
+    public float Low_Damp;
     // Start is called before the first frame update
 
     // Update is called once per frame
@@ -17,16 +20,19 @@ public class Camera : MonoBehaviour
     {
         if (Player.position.y >= Height_Threshold)
         {
+            Current_SmoothDamp = High_Damp;
             Current_Offset = High_Offset;
         } 
         else if (Player.position.y < Height_Threshold)
         {
+            Current_SmoothDamp = Low_Damp;
             Current_Offset = Normal_Offset;
         }
 
         Vector3 TargetPosition = Player.position + Current_Offset;
-        Vector3 SmoothedPosition = Vector3.Lerp(transform.position, TargetPosition, SmoothDamp);
+        Vector3 SmoothedPosition = Vector3.Lerp(transform.position, TargetPosition, Current_SmoothDamp);
         transform.position = SmoothedPosition;
+
 
 
 
