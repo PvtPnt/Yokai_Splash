ddf9cdd3e2c653f80f069fdc2bf16b96ead22561
nakamuraz0 <44020815+nakamuraz0@users.noreply.github.com>
2020-01-31 20:09:24 +0700

    DialogueBox

---
+++ Assets/Script/DialogueBox.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueBox : MonoBehaviour
+{
+
+    public Sprite charPic;
+    public string charName;
+    public string message;
+
+    //Obj Area
+    public Image charPicObj;
+    public Text charNameObj;
+    public Text messageObj;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        charPicObj.sprite = charPic;
+        charNameObj.text = charName;
+        messageObj.text = message;
+        Time.timeScale = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.JoystickButton0))
+        {
+            Time.timeScale = 1;
+            Destroy(this.gameObject);
+        }
+    }
+}
