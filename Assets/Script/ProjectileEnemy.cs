f211de4e191a5f33b2ae19cc2ab52603e4422229
PisitSuengthaworn <pisit_s@sci.tu.ac.th>
2020-02-17 16:12:12 +0700

    fixed jumpin bug, fixed ranged enemy

--- Assets/Script/ProjectileEnemy.cs
+++ Assets/Script/ProjectileEnemy.cs
@@ -4,14 +4,14 @@ using UnityEngine;
 
 public class ProjectileEnemy : MonoBehaviour
 {
-    public int HP;
 
-    public float WalkSpeed;
-    public float Tsuchinoko_Jumpforce;
-    public float groundCheckRange = 1f;
-    public float wallCheckRange = 1f;
-    public float EnemyCheckRange = 10f;
-    public float AttackRange;
+
+    //public float WalkSpeed;
+    //public float Tsuchinoko_Jumpforce;
+    //public float groundCheckRange = 1f;
+    //public float wallCheckRange = 1f;
+    //public float EnemyCheckRange = 10f;
+    //public float AttackRange;
 
     public int Damage;
 
@@ -24,14 +24,15 @@ public class ProjectileEnemy : MonoBehaviour
     public LayerMask playerLayer;
 
     public Transform EnemyHitbox;
-    public Transform Current_WallChecker;
-    public Transform WallChecker_Right;
-    public Transform WallChecker_Left;
+    //public Transform Current_WallChecker;
+    //public Transform WallChecker_Right;
+    //public Transform WallChecker_Left;
 
     public SpriteRenderer TsuchinokoSprite;
 
     public bool isPlayerinRange = false;
     public float stateDelay;
+    private GameObject playerObj;
 
     //Projectile Behavior
     public Transform spawnPoint;
@@ -47,14 +48,14 @@ public class ProjectileEnemy : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
-    { if (HP <= 0) { Destroy(gameObject); } }
+    //void Update()
+    ////{ if (HP <= 0) { Destroy(gameObject); } }
 
     void FixedUpdate()
     {
         if (!isPlayerinRange)
         {
-            Walking();
+            return;
         }
         else
         {
@@ -69,88 +70,90 @@ public class ProjectileEnemy : MonoBehaviour
     }
 
 
-    public void ReceiveDamage(int Damage)
-    {
-        HP -= Damage;
-        Debug.Log("Damage taken");
-    }
+    //public void ReceiveDamage(int Damage)
+    //{
+    //    HP -= Damage;
+    //    Debug.Log("Damage taken");
+    //}
 
-    void Walking()
-    {
-        CheckWall();
-        isGrounded = Physics2D.OverlapCircle(transform.position, groundCheckRange, groundLayer);
-        if (isGrounded)
-        {
-            if (IsWalkingLeft == true)
-            { StartCoroutine("Tsuchinoko_MoveLeft", 0.45f); }
-            else if (IsWalkingLeft == false)
-            { StartCoroutine("Tsuchinoko_MoveRight", 0.45f); }
-        }
-    }
+    //void Walking()
+    //{
+    //    CheckWall();
+    //    isGrounded = Physics2D.OverlapCircle(transform.position, groundCheckRange, groundLayer);
+    //    if (isGrounded)
+    //    {
+    //        if (IsWalkingLeft == true)
+    //        { StartCoroutine("Tsuchinoko_MoveLeft", 0.45f); }
+    //        else if (IsWalkingLeft == false)
+    //        { StartCoroutine("Tsuchinoko_MoveRight", 0.45f); }
+    //    }
+    //}
 
-    void CheckWall()
-    {
-        if (IsWalkingLeft == true)
-        {
-            Current_WallChecker = WallChecker_Left;
-            TsuchinokoSprite.flipX = false;
-        }
-        else if (IsWalkingLeft == false)
-        {
-            Current_WallChecker = WallChecker_Right;
-            TsuchinokoSprite.flipX = true;
-        }
+    //void CheckWall()
+    //{
+    //    if (IsWalkingLeft == true)
+    //    {
+    //        Current_WallChecker = WallChecker_Left;
+    //        TsuchinokoSprite.flipX = false;
+    //    }
+    //    else if (IsWalkingLeft == false)
+    //    {
+    //        Current_WallChecker = WallChecker_Right;
+    //        TsuchinokoSprite.flipX = true;
+    //    }
 
-        isWalled = Physics2D.OverlapCircle(Current_WallChecker.position, wallCheckRange, wallLayer);
+    //    isWalled = Physics2D.OverlapCircle(Current_WallChecker.position, wallCheckRange, wallLayer);
 
-        if (isWalled == true)
-        {
-            if (IsWalkingLeft == true)
-            { IsWalkingLeft = false; }
-            else if (IsWalkingLeft == false)
-            { IsWalkingLeft = true; }
-        }
-    }
+    //    if (isWalled == true)
+    //    {
+    //        if (IsWalkingLeft == true)
+    //        { IsWalkingLeft = false; }
+    //        else if (IsWalkingLeft == false)
+    //        { IsWalkingLeft = true; }
+    //    }
+    //}
 
-    IEnumerator Tsuchinoko_MoveLeft(float WaitTime)
-    {
-        GetComponent<Rigidbody2D>().AddForce(Vector2.left * WalkSpeed * Time.deltaTime, ForceMode2D.Force);
-        GetComponent<Rigidbody2D>().AddForce(Vector2.up * Tsuchinoko_Jumpforce, ForceMode2D.Impulse);
-        Vector3 temp = spawnPoint.localScale;
-        temp.x *= -1;
-        spawnPoint.localScale = temp;
-        yield return new WaitForSecondsRealtime(WaitTime);
-    }
+    //IEnumerator Tsuchinoko_MoveLeft(float WaitTime)
+    //{
+    //    GetComponent<Rigidbody2D>().AddForce(Vector2.left * WalkSpeed * Time.deltaTime, ForceMode2D.Force);
+    //    GetComponent<Rigidbody2D>().AddForce(Vector2.up * Tsuchinoko_Jumpforce, ForceMode2D.Impulse);
+    //    Vector3 temp = spawnPoint.localScale;
+    //    temp.x *= -1;
+    //    spawnPoint.localScale = temp;
+    //    yield return new WaitForSecondsRealtime(WaitTime);
+    //}
 
-    IEnumerator Tsuchinoko_MoveRight(float WaitTime)
-    {
-        GetComponent<Rigidbody2D>().AddForce(Vector2.right * WalkSpeed * Time.deltaTime, ForceMode2D.Force);
-        GetComponent<Rigidbody2D>().AddForce(Vector2.up * Tsuchinoko_Jumpforce, ForceMode2D.Impulse);
-        Vector3 temp = spawnPoint.localScale;
-        temp.x = Mathf.Abs(temp.x);
-        spawnPoint.localScale = temp;
-        yield return new WaitForSecondsRealtime(WaitTime);
-    }
+    //IEnumerator Tsuchinoko_MoveRight(float WaitTime)
+    //{
+    //    GetComponent<Rigidbody2D>().AddForce(Vector2.right * WalkSpeed * Time.deltaTime, ForceMode2D.Force);
+    //    GetComponent<Rigidbody2D>().AddForce(Vector2.up * Tsuchinoko_Jumpforce, ForceMode2D.Impulse);
+    //    Vector3 temp = spawnPoint.localScale;
+    //    temp.x = Mathf.Abs(temp.x);
+    //    spawnPoint.localScale = temp;
+    //    yield return new WaitForSecondsRealtime(WaitTime);
+    //}
 
-    private void OnDrawGizmosSelected()
-    {
-        Vector3 Direction = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(Current_WallChecker.position, wallCheckRange);
-        if (IsWalkingLeft == false)
-        { Gizmos.DrawWireSphere(EnemyHitbox.position, AttackRange); }
+    //private void OnDrawGizmosSelected()
+    //{
+    //    Vector3 Direction = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
+    //    Gizmos.color = Color.red;
+    //    Gizmos.DrawWireSphere(Current_WallChecker.position, wallCheckRange);
+    //    if (IsWalkingLeft == false)
+    //    { Gizmos.DrawWireSphere(EnemyHitbox.position, AttackRange); }
 
-        if (IsWalkingLeft == true)
-        { Gizmos.DrawWireSphere(EnemyHitbox.position, AttackRange); }
-    }
-    void OnCollisionEnter2D(Collision2D other)
+    //    if (IsWalkingLeft == true)
+    //    { Gizmos.DrawWireSphere(EnemyHitbox.position, AttackRange); }
+    //}
+
+    private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.tag == "Player")
         {
             isPlayerinRange = true;
+            playerObj = other.gameObject;
         }
     }
-    void OnCollisionExit2D(Collision2D other)
+    private void OnTriggerExit2D(Collider2D other)
     {
         if (other.gameObject.tag == "Player")
         {
@@ -159,13 +162,16 @@ public class ProjectileEnemy : MonoBehaviour
             currentShotDelay = 0;
         }
     }
+    
 
-    void DelayAttacktoIdle()
-    {
-        isPlayerinRange = false;
-    }
+    //void DelayAttacktoIdle()
+    //{
+    //    isPlayerinRange = false;
+    //}
     void EnemyShoot()
     {
-       Instantiate(bullet, spawnPoint);
+       float angle = Vector2.Angle(Vector2.right, playerObj.transform.position - spawnPoint.position);
+       Quaternion rot = Quaternion.Euler(0f, 0f, angle);
+       Instantiate(bullet, spawnPoint.position, rot);
     }
 }
