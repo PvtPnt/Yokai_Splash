c531ba91bb64c23c84d3c53cb1a90ee076022491
PvtPnt <44025250+PvtPnt@users.noreply.github.com>
2020-03-27 12:32:50 +0700

    Revert "Revert "Revert "Merge branch 'Function' into Merge"""
    This reverts commit 373de2b42b70a23a81d4c598368475368e1819f5.

---
+++ Assets/Script/Camera.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    public Transform Player;
+    public float Height_Threshold;
+    public Vector3 Normal_Offset;
+    public Vector3 Current_Offset;
+    public Vector3 High_Offset;
+
+    public float SmoothDamp;
+
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Player.position.y >= Height_Threshold)
+        {Current_Offset = High_Offset;} 
+        else if (Player.position.y < Height_Threshold)
+        {Current_Offset = Normal_Offset;}
+
+        Vector3 TargetPosition = Player.position + Current_Offset;
+        Vector3 SmoothedPosition = Vector3.Lerp(transform.position, TargetPosition, SmoothDamp);
+        transform.position = SmoothedPosition;
+        }
+}
+
--- Assets/Script/Onikuma.cs
+++ Assets/Script/Onikuma.cs
@@ -4,29 +4,23 @@ using UnityEngine;
 
 public class Onikuma : MonoBehaviour
 {
+    //public int HP;
+
     public float WalkSpeed;
     public float RushSpeed;
     public float CooldownTimer;
 
-    public float WalkStepLength;
-    public float RushStepLength;
-    public float ClawStepLength;
     public float groundCheckRange = 1f;
     public float wallCheckRange = 1f;
     public float EnemyCheckRange = 10f;
     public float AttackRange;
     public float Col_sizeX, Col_sizeY = 1f;
     public float Col_OffsetX, Col_OffsetY;
-    public float HighThrow_offset;
-    public float LowThrow_offset;
 
     public BoxCollider2D BCollider2D;
 
     public int Damage;
     public int ActionIndex;
-    public int ActionMin;
-    public int ActionMax;
-    public int WalkStepCount;
 
     public bool IsWalkingLeft;
     public bool isGrounded;
@@ -39,39 +33,42 @@ public class Onikuma : MonoBehaviour
 
     public Transform EnemyHitbox;
     public Transform groundChecker;
-    public Transform AttackStartPosition;
-    public Transform AttackEndPosition;
+    public Transform Current_WallChecker;
+    public Transform WallChecker_Right;
+    public Transform WallChecker_Left;
 
-    SpriteRenderer OnikumaSprite;
-    public Sprite Standing;
-    public Sprite Prowling;
-    public GameObject Throwables;
+    public Transform RoomEnd_L;
+    public Transform RoomEnd_R;
 
-    public Vector3 PlayerPosition;
-    private float PlayerPosition_X;
+    public SpriteRenderer OnikumaSprite;
+
+    public Vector2 PlayerPosition;
     // Start is called before the first frame update
     void Start()
     {
         BCollider2D = GetComponent<BoxCollider2D>();
-        OnikumaSprite = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     { 
+        //if (HP <= 0) { Destroy(gameObject); }
         BCollider2D.size = new Vector3(Col_sizeX, Col_sizeY, 0);
         BCollider2D.offset = new Vector3(Col_OffsetX, Col_OffsetY, 0);
-
-            Collider2D[] DamagePlayer = Physics2D.OverlapCircleAll(EnemyHitbox.position, AttackRange, playerLayer);
-            for (int i = 0; i < DamagePlayer.Length; i++)
-            { DamagePlayer[i].GetComponent<Player_cube_control>().P_ReceiveDamage(Damage); }
     }
 
     void FixedUpdate()
     {
         PlayerPosition = GameObject.Find("Player").transform.position;
         if (isPerformingAction == false) { StartCoroutine("ActionManager"); }
+        Collider2D[] DamagePlayer = Physics2D.OverlapCircleAll(EnemyHitbox.position, AttackRange, playerLayer);
+        for (int i = 0; i < DamagePlayer.Length; i++)
+        { DamagePlayer[i].GetComponent<Player_cube_control>().P_ReceiveDamage(Damage); }
+
     }
+
+    //public void ReceiveDamage(int Damage)
+    //{HP -= Damage;}
 
     IEnumerator ActionCooldown()
     {
@@ -79,142 +76,128 @@ public class Onikuma : MonoBehaviour
         Debug.Log("Action in cooldown");
         yield return new WaitForSeconds(CooldownTimer);
         isPerformingAction = false;
-        WalkStepCount = 0;
-        Col_OffsetX = -0.017f;
-        Col_OffsetY = 0.0597f;
-        Col_sizeX = 4.82f;
-        Col_sizeY = 7.58f;
     }
 
     IEnumerator ReturnCollider()
     {
         yield return new WaitForSeconds(1f);
-        AttackRange = 3.5f;
+        AttackRange = 1f;
+        Col_sizeX = 1f;
+        Col_sizeY = 1f;
         yield return null;
     }
 
     IEnumerator ActionManager()
     {
-        OnikumaSprite.sprite = Standing;
         isPerformingAction = true;
-        ActionIndex = Random.Range(ActionMin, ActionMax); //Set ActionIndex value to random number between (a,b)} 
+        ActionIndex = Random.Range(1, 4); //Set ActionIndex value to random number between (a,b)} 
         Debug.Log("Action index is " + ActionIndex);
-        if      (ActionIndex <= 2)  { InvokeRepeating("Walking",0.5f,1f); }
-        else    { InvokeRepeating("GoToAttackStart", 0.5f, 1f); }
+        if      (ActionIndex <= 2)  { Walking(); }
+        else if (ActionIndex == 3)  { RushAttack(); }
+        else if (ActionIndex == 4)  { Claw(); }
+        else if (ActionIndex == 5)  { SlipRush(); }
+        else if (ActionIndex == 6)  { Throw(); }
 
+        //yield return StartCoroutine("ActionCooldown");
         yield return null;
     }
 
     void Walking()
     {
-        WalkStepCount += 1;
-        if (WalkStepCount <= 5)
-        {
-            Debug.Log("Walking");
-            transform.position = Vector3.MoveTowards(transform.position, new Vector3(PlayerPosition.x, transform.position.y, 0), WalkStepLength);
-        }
-        else
-        {
-            CancelInvoke("Walking");
-            StartCoroutine("ActionCooldown");
-        }
-    }
-
-    void GoToAttackStart()
-    {
-        transform.position = Vector3.MoveTowards(transform.position, new Vector3(AttackStartPosition.position.x, transform.position.y, 0), WalkStepLength);
-        if ( transform.position.x == AttackStartPosition.position.x)
-        {
-            CancelInvoke("GoToAttackStart");
-            if (ActionIndex == 3) { InvokeRepeating("RushAttack",0.5f,0.25f); }
-            else if (ActionIndex == 4) { InvokeRepeating("Claw", 0.5f, 0.25f); }
-            else if (ActionIndex == 5) { ThrowHigh(); }
-            else if (ActionIndex == 6) { ThrowLow(); }
-        }
+        Debug.Log("Walking");
+        if (transform.position.x < PlayerPosition.x)
+        { StartCoroutine("Onikuma_MoveRight", 3.0f); }
+        else if (transform.position.x > PlayerPosition.x)
+        { StartCoroutine("Onikuma_MoveLeft", 3.0f); }
     }
 
     void RushAttack()
     {
-        OnikumaSprite.sprite = Prowling;
-        Col_OffsetX = -0.016f;
-        Col_OffsetY = 0.369f;
-        Col_sizeX = 4.82f;
-        Col_sizeY = 2.46f;
+        Vector2 PlayerPosition = GameObject.Find("Player").transform.position;
+        Col_sizeX = 2f;
+        Col_sizeY = 0.5f;
 
-        if (transform.position.x != AttackEndPosition.position.x)
-        { transform.position = Vector3.MoveTowards(transform.position, new Vector3(AttackEndPosition.position.x, transform.position.y, 0), RushStepLength); }
-        else
-        { 
-            CancelInvoke("RushAttack");
-            InvokeRepeating("RushBack", 1f, 0.25f);
-        }
-    }
-
-    void RushBack()
-    {
-        if (transform.position.x != AttackStartPosition.position.x)
-        { transform.position = Vector3.MoveTowards(transform.position, new Vector3(AttackStartPosition.position.x, transform.position.y, 0), RushStepLength); }
-        else
+        if (transform.position.x < PlayerPosition.x) //Player is to the right
         {
-            CancelInvoke("RushBack");
-            StartCoroutine("ActionCooldown");
+            Debug.Log("Execute RushAttack Right");
+            RoomEndAttack_Right();
+        }
+        else if (transform.position.x > PlayerPosition.x) //Player is to the left
+        {
+            Debug.Log("Execute RushAttack Left");
+            RoomEndAttack_Left();
         }
     }
 
     void Claw()
     {
-        AttackRange = 3f;
-        Col_OffsetX = -0.017f;
-        Col_OffsetY = 0.0597f;
-        Col_sizeX = 4.82f;
-        Col_sizeY = 7.58f;
+        Vector2 PlayerPosition = GameObject.Find("Player").transform.position;
+        AttackRange = 2f;
+        Col_sizeX = 0.5f;
+        Col_sizeY = 2f;
         Debug.Log("Execute Claw");
 
-        if (transform.position.x != AttackEndPosition.position.x)
-        { transform.position = Vector3.MoveTowards(transform.position, new Vector3(AttackEndPosition.position.x, transform.position.y, 0), ClawStepLength); }
-        else
+        if (transform.position.x < PlayerPosition.x) //Player is to the right
         {
-            CancelInvoke("Claw");
-            InvokeRepeating("ClawBack", 1f, 0.25f);
+            Debug.Log("Execute ClawAttack Right");
+            RoomEndAttack_Right();
         }
-
-    }
-
-    void ClawBack()
-    {
-        if (transform.position.x != AttackStartPosition.position.x)
-        { transform.position = Vector3.MoveTowards(transform.position, new Vector3(AttackStartPosition.position.x, transform.position.y, 0), RushStepLength); }
-        else
+        else if (transform.position.x > PlayerPosition.x) //Player is to the left
         {
-            CancelInvoke("ClawBack");
-            StartCoroutine("ActionCooldown");
+            Debug.Log("Execute ClawAttack Left");
+            RoomEndAttack_Left();
         }
     }
 
-    void ThrowHigh()
+    void SlipRush()
     {
-        Debug.Log("Execute High Throw");
-        GameObject Throwabless =
-        Instantiate(Throwables, transform.position + new Vector3(0, HighThrow_offset,0) , Quaternion.identity);
-
-        Vector2 PlayerPosition = GameObject.Find("Player").transform.position;
-        
-        if (transform.position.x < PlayerPosition.x)
-        { Throwables.GetComponent<OnikumaThrowables>().isMovingLeft = false; }
-        else { Throwables.GetComponent<OnikumaThrowables>().isMovingLeft = true; }
+        Debug.Log("Execute SlipRush");
         StartCoroutine("ActionCooldown");
     }
 
-    void ThrowLow()
+    void Throw()
     {
-        Debug.Log("Execute Low Throw");
-        GameObject Throwabless =
-        Instantiate(Throwables, transform.position + new Vector3(0, LowThrow_offset, 0), Quaternion.identity);
-        
-        if (transform.position.x < PlayerPosition.x)
-        { Throwables.GetComponent<OnikumaThrowables>().isMovingLeft = false; }
-        else { Throwables.GetComponent<OnikumaThrowables>().isMovingLeft = true; }
+        Debug.Log("Execute Throw");
         StartCoroutine("ActionCooldown");
+    }
+
+
+    //void CheckWall()
+    //{
+    //    if (IsWalkingLeft == true)
+    //    {
+    //        Current_WallChecker = WallChecker_Left;
+    //        OnikumaSprite.flipX = false;
+    //    }
+    //    else if (IsWalkingLeft == false)
+    //    {
+    //        Current_WallChecker = WallChecker_Right;
+    //        OnikumaSprite.flipX = true;
+    //    }
+
+    //    isWalled = Physics2D.OverlapCircle(Current_WallChecker.position, wallCheckRange, wallLayer);
+
+    //    if (isWalled == true)
+    //    {
+    //        if (IsWalkingLeft == true)
+    //        { IsWalkingLeft = false; }
+    //        else if (IsWalkingLeft == false)
+    //        { IsWalkingLeft = true; }
+    //    }
+    //}
+
+    IEnumerator Onikuma_MoveLeft(float WaitTime)
+    {
+        GetComponent<Rigidbody2D>().AddForce(Vector2.left * WalkSpeed, ForceMode2D.Force);
+        yield return new WaitForSeconds(WaitTime);
+        StartCoroutine("ActionCooldown");
+    }
+
+    IEnumerator Onikuma_MoveRight(float WaitTime)
+    {
+        GetComponent<Rigidbody2D>().AddForce(Vector2.right * WalkSpeed, ForceMode2D.Force);
+        yield return new WaitForSeconds(WaitTime);
+        yield return StartCoroutine("ActionCooldown");
     }
 
     void RoomEndAttack_Left()
@@ -237,18 +220,22 @@ public class Onikuma : MonoBehaviour
         StartCoroutine("ActionCooldown");
     }
 
-    private void OnDrawGizmosSelected()
-    {
-        Vector3 Direction = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
 
-        Gizmos.color = Color.green;
-        Gizmos.DrawWireSphere(groundChecker.position, groundCheckRange);
 
-        Gizmos.color = Color.blue;
-        if (IsWalkingLeft == false)
-        { Gizmos.DrawWireSphere(EnemyHitbox.position, AttackRange); }
+    //private void OnDrawGizmosSelected()
+    //{
+    //    Vector3 Direction = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
+    //    Gizmos.color = Color.red;
+    //    Gizmos.DrawWireSphere(Current_WallChecker.position, wallCheckRange);
 
-        if (IsWalkingLeft == true)
-        { Gizmos.DrawWireSphere(EnemyHitbox.position, AttackRange); }
-    }
+    //    Gizmos.color = Color.green;
+    //    Gizmos.DrawWireSphere(groundChecker.position, groundCheckRange);
+
+    //    Gizmos.color = Color.blue;
+    //    if (IsWalkingLeft == false)
+    //    { Gizmos.DrawWireSphere(EnemyHitbox.position, AttackRange);}
+
+    //    if (IsWalkingLeft == true)
+    //    {Gizmos.DrawWireSphere(EnemyHitbox.position, AttackRange);}
+    //}
 }
