373de2b42b70a23a81d4c598368475368e1819f5
PvtPnt <44025250+PvtPnt@users.noreply.github.com>
2020-03-27 12:30:32 +0700

    Revert "Revert "Merge branch 'Function' into Merge""
    This reverts commit 5d01b7cf55e3d63208ca563b7821958443fb3925.

--- Assets/Asset/Title screen asset/Title script/MainMenu.cs
+++ Assets/Asset/Title screen asset/Title script/MainMenu.cs
@@ -22,13 +22,13 @@ public class MainMenu : MonoBehaviour
     void OnMouseUp()
     {
         if (isStart)
-        {SceneManager.LoadScene("proto1");}
+        {SceneManager.LoadScene("level 1");}
         if (isQuit)
         {Application.Quit();}
     }
 
     public void LoadLevel()
     {
-        SceneManager.LoadScene("proto1");
+        SceneManager.LoadScene("level 1");
     }
 }
---
+++ Assets/Script/CameraMovement.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public Transform Player;
+    public float Height_Threshold;
+    public Vector3 Normal_Offset;
+    public Vector3 Current_Offset;
+    public Vector3 High_Offset;
+
+    public bool isAtBoss = false;
+    public int BossNumber;
+    public Vector3 LV1BossCam;
+    public Vector3 LV2BossCam;
+    public Vector3 LV3BossCam;
+    public float SmoothDamp;
+
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (isAtBoss == false)
+        {
+            if (Player.position.y >= Height_Threshold)
+            { Current_Offset = High_Offset; }
+            else if (Player.position.y < Height_Threshold)
+            { Current_Offset = Normal_Offset; }
+
+            Vector3 TargetPosition = Player.position + Current_Offset;
+            Vector3 SmoothedPosition = Vector3.Lerp(transform.position, TargetPosition, SmoothDamp);
+            transform.position = SmoothedPosition;
+        }
+        else
+        {
+            if (BossNumber == 1)
+            {
+                Vector3 SmoothedPosition = Vector3.Lerp(transform.position, LV1BossCam, SmoothDamp);
+                transform.position = SmoothedPosition;
+            }
+            else if (BossNumber == 2)
+            {
+                Vector3 SmoothedPosition = Vector3.Lerp(transform.position, LV2BossCam, SmoothDamp);
+                transform.position = SmoothedPosition;
+            }
+            else if (BossNumber == 3)
+            {
+                Vector3 SmoothedPosition = Vector3.Lerp(transform.position, LV3BossCam, SmoothDamp);
+                transform.position = SmoothedPosition;
+            }
+
+        }
+    }
+}
+
