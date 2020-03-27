d4a5bd330f839c2f0a0f146e926a642a55b107e9
PvtPnt <44025250+PvtPnt@users.noreply.github.com>
2020-03-28 02:19:00 +0700

    Title Menu controls with Controller

--- Assets/Asset/Title screen asset/Title script/ButtonScirpt.cs
+++ Assets/Asset/Title screen asset/Title script/ButtonScirpt.cs
@@ -5,21 +5,27 @@ using UnityEngine;
 public class ButtonScirpt : MonoBehaviour
 {
     public SpriteRenderer ButtonSprite;
+    public bool isSelected;
+    public int ButtonNumber;
     // Start is called before the first frame update
     void Start()
     {
-       
+        ButtonSprite = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (isSelected)
+        {
+            ButtonSprite.color = Color.red;
+        }
+        else { ButtonSprite.color = Color.white; }
     }
 
     void OnMouseEnter()
-    {ButtonSprite.color = Color.red;}
+    { isSelected = true; }
 
     void OnMouseExit()
-    {ButtonSprite.color = Color.white;}
+    { isSelected = false; }
 }
--- Assets/Asset/Title screen asset/Title script/MainMenu.cs
+++ Assets/Asset/Title screen asset/Title script/MainMenu.cs
@@ -5,19 +5,64 @@ using UnityEngine;
 
 public class MainMenu : MonoBehaviour
 {
+    public float InputValue;
+    public int MenuIndex;
+
+    public bool isHoldingLeftStick;
     public bool isStart;
     public bool isQuit;
+
+    public SpriteRenderer ButtonSprite;
+    public bool isSelected;
+    public int ButtonNumber;
     // Start is called before the first frame update
     void Start()
     {
-        
+        ButtonSprite = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (isSelected)
+        { ButtonSprite.color = Color.red; }
+        else { ButtonSprite.color = Color.white; }
+
+        if (MenuIndex > 2) { MenuIndex = 2; }
+        else if (MenuIndex < 0) { MenuIndex = 0; }
+
+        InputValue = Input.GetAxisRaw("Vertical");
+        if (InputValue != 0)
+        {
+            if(isHoldingLeftStick == false)
+            {
+                if(InputValue > 0f) { MenuIndex -= 1; }
+                else if (InputValue < 0f) { MenuIndex += 1; }
+                isHoldingLeftStick = true;
+            }
+        }
+
+        if (MenuIndex == ButtonNumber)
+        { isSelected = true; }
+        else { isSelected = false; }
+
+        if (InputValue == 0) { isHoldingLeftStick = false; }
+
+        if (isSelected == true && Input.GetKeyDown(KeyCode.JoystickButton0))
+        {
+            if (isStart) { SceneManager.LoadScene("level 1"); }
+            if (isQuit) { Application.Quit(); }
+        }
+
+
+
     }
+
+    void OnMouseEnter()
+    { isSelected = true; }
+
+    void OnMouseExit()
+    { isSelected = false; }
 
     void OnMouseUp()
     {
