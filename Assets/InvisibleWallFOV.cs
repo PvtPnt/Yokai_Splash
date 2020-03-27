c87ade3dc09059c17cb752dfd9f7ada9933195bc
PvtPnt <44025250+PvtPnt@users.noreply.github.com>
2020-03-27 14:26:54 +0700

    Import missing scripts

---
+++ Assets/InvisibleWallFOV.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InvisibleWallFOV : MonoBehaviour
+{
+    public Camera MainCamera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        MainCamera = GameObject.Find("Main Camera").GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            print("Collide Player");
+            print(MainCamera.fieldOfView);
+            MainCamera.fieldOfView = 80;
+        }
+        else
+        {
+            MainCamera.fieldOfView = 60;
+        }
+    }
+    private void OnDestroy()
+    {
+        MainCamera.fieldOfView = 60;
+    }
+}
