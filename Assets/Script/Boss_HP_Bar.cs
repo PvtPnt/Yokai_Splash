8984d32960c92b90e150ae9c0ca6702ebc7e6f63
PvtPnt <44025250+PvtPnt@users.noreply.github.com>
2020-05-09 11:02:08 +0700

    Add boss HP bar

---
+++ Assets/Script/Boss_HP_Bar.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss_HP_Bar : MonoBehaviour
+{
+    public Transform Bar;
+    public GameObject Boss;
+    [SerializeField] float HP;
+    [SerializeField] float MaxHP;
+    [SerializeField] float HP_Percentage;
+    // Start is called before the first frame update
+    void Start()
+    {
+        MaxHP = Boss.GetComponent<Enemy_hp>().HP;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HP = Boss.GetComponent<Enemy_hp>().HP;
+        HP_Percentage = HP / MaxHP;
+
+        Bar.localScale = new Vector3(HP_Percentage, 1f);
+    }
+}
