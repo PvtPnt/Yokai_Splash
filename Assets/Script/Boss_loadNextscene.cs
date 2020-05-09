25a3115c2870b050731c0ab9e9901ddfe4ce9017
PvtPnt <44025250+PvtPnt@users.noreply.github.com>
2020-05-09 10:45:53 +0700

    Load Next Scene when boss die
    Press [E] or Button [A] to load next scene when boss die

---
+++ Assets/Script/Boss_loadNextscene.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Boss_loadNextscene : MonoBehaviour
+{
+    public GameObject Boss;
+    public string Scene_to_load;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Boss == null && Input.GetKeyDown(KeyCode.E) || Boss == null && Input.GetKeyDown(KeyCode.JoystickButton0))
+        {SceneManager.LoadScene(Scene_to_load);}
+    }
+}
